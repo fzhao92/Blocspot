ecd65e148cacfd450f740e67ae65144736a207f8
Forrest Zhao <fozhao92@gmail.com>
2016-05-25 23:23:08 -0700

    add mapview, mapview iboutlet & delegates, wip: search bar view controller

---
+++ Blocspot/Blocspot/SearchResultsTable.swift
@@ -0,0 +1,14 @@
+//
+//  SearchResultsTable.swift
+//  Blocspot
+//
+//  Created by Forrest Zhao on 5/25/16.
+//  Copyright © 2016 Forrest Zhao. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class SearchResultsTable: UITableViewController {
+    <#code#>
+}
--- Blocspot/Blocspot/ViewController.swift
+++ Blocspot/Blocspot/ViewController.swift
@@ -7,12 +7,20 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
-
+    let locationManager = CLLocationManager()
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.requestLocation()
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +28,24 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
 }
 
+extension ViewController : CLLocationManagerDelegate {
+    func locationManager(manager: CLLocationManager, didChangeAuthorizationStatus status: CLAuthorizationStatus) {
+        if status == .AuthorizedWhenInUse {
+            locationManager.requestLocation()
+        }
+    }
+    
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.first {
+            let span = MKCoordinateSpanMake(0.05, 0.05)
+            let region = MKCoordinateRegion(center: location.coordinate, span: span)
+            mapView.setRegion(region, animated: true)
+        }
+    }
+    
+    func locationManager(manager: CLLocationManager, didFailWithError error: NSError) {
+        print("error:: \(error)")
+    }
+}
